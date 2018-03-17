369e3b4564ad6cc96463495f538f07ebfbe1029d
kai <kfan20911@gmail.com>
2018-03-16 20:55:21 -0400

    update

--- src/components/product/ProductItem.vue
+++ src/components/product/ProductItem.vue
@@ -4,7 +4,7 @@
     <v-card-media :src=product.image height="400px" contain></v-card-media>
     <v-card-title primary-title>
       <div>
-        <h3 class="headline mb-0">{{product.name}}</h3>
+        <h3 class="headline mb-0 text-md-center">{{product.name}}</h3>
       </div>
     </v-card-title>
     <v-card-actions>
